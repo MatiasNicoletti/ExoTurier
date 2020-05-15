d41312a21b91e3a820ca44d9c0cccdf9b861bd01
Matias Nicoletti <matiasnicolettmdp@gmail.com>
2020-05-15 19:09:50 -0300

    First Commit

---
+++ exotourier/src/main/java/com/exotourier/exotourier/ExotourierApplication.java
@@ -0,0 +1,13 @@
+package com.exotourier.exotourier;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExotourierApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExotourierApplication.class, args);
+	}
+
+}
