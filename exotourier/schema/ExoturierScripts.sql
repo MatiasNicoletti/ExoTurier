b8ec11dff10d3404db15d9428f105e4f41a85dae
Matias Nicoletti <matiasnicolettmdp@gmail.com>
2020-05-26 12:14:43 -0300

    schema modifiedç

--- exotourier/schema/ExoturierScripts.sql
+++ exotourier/schema/ExoturierScripts.sql
@@ -1,23 +1,26 @@
+DROP DATABASE exoturier;
 CREATE DATABASE exoturier;
 USE exoturier;
 
+
+
 CREATE TABLE `countries` (
-	`id` INT NOT NULL,
-    `name` VARCHAR(255) NOT NULL UNIQUE,
+	`id` INT AUTO_INCREMENT,
+    `country_name` VARCHAR(255) NOT NULL UNIQUE,
     CONSTRAINT `Pk_country` PRIMARY KEY (`id`)
 );
 
 CREATE TABLE `cities` (
-	`id` INT NOT NULL,
-    `name` VARCHAR(255) NOT NULL UNIQUE,
+	`id` INT AUTO_INCREMENT,
+    `city_name` VARCHAR(255) NOT NULL UNIQUE,
     `id_country` INT NOT NULL,
     CONSTRAINT `Pk_city` PRIMARY KEY (`id`),
     CONSTRAINT `Fk_cities_country` FOREIGN KEY(`id_country`) REFERENCES `countries`(`id`)
 );
 
 CREATE TABLE `excursions` (
-	`id` INT NOT NULL,
-    `name` VARCHAR(255) NOT NULL,
+	`id` INT AUTO_INCREMENT,
+    `excursion_name` VARCHAR(255) NOT NULL,
     `description` TEXT NOT NULL,	
     `price` FLOAT NOT NULL,
     `place` VARCHAR(255),
@@ -28,25 +31,30 @@ CREATE TABLE `excursions` (
 );
 
 CREATE TABLE `users` (
-	`id` INT NOT NULL,
-    `name` VARCHAR(255) NOT NULL,
+	`id` INT AUTO_INCREMENT,
+    `firstname` VARCHAR(255) NOT NULL,
     `surname` VARCHAR(255) NOT NULL,
-    `email` VARCHAR(255) NOT NULL UNIQUE,
+    `email` VARCHAR(255) NOT NULL,
     `password` VARCHAR(255),
     `is_active` BOOLEAN DEFAULT TRUE NOT NULL,
-    CONSTRAINT `Pk_user` PRIMARY KEY (`id`)
+    CONSTRAINT `Pk_user` PRIMARY KEY (`id`),
+    CONSTRAINT `Unq_user_email` UNIQUE (email)
 );
-
+insert into users(firstname, surname, email, password, is_active) values('attila', 'the hun', 'attila@gmail.com', '123456', true);
+select * from users;
 CREATE TABLE `users_x_excursions` (
+    `id` INT AUTO_INCREMENT,
 	`id_user` INT NOT NULL,
     `id_excursion` INT NOT NULL,
+    `rating` INT,
+    CONSTRAINT `Pk_users_x_excursions` PRIMARY KEY (id),
 	CONSTRAINT `Fk_users_x_excursions__id_user` FOREIGN KEY(`id_user`) REFERENCES `users`(`id`),
     CONSTRAINT `Fk_users_x_excursions__id_excursion` FOREIGN KEY(`id_excursion`) REFERENCES `excursions`(`id`)
 );
 
 CREATE TABLE `purchases` (
-	`id` INT NOT NULL,
-    `date` DATE NOT NULL,
+	`id` INT AUTO_INCREMENT,
+    `purchase_date` DATE NOT NULL,
     `total_price` FLOAT NOT NULL,
     `id_excursion` INT NOT NULL,
     `id_user` INT NOT NULL,
@@ -54,3 +62,16 @@ CREATE TABLE `purchases` (
     CONSTRAINT `Fk_purchases_id_excursion` FOREIGN KEY(`id_excursion`) REFERENCES `excursions`(`id`),
     CONSTRAINT `Fk_purchases_id_user` FOREIGN KEY(`id_user`) REFERENCES `users`(`id`)
 );
+
+
+select * from purchases;
+select * from excursions;
+select * from users_x_excursions;
+insert into countries(country_name) values('Burkina Fasso');
+insert into cities(city_name, id_country) values('Capital', 1);
+
+insert into excursions(excursion_name, description, price, place, id_city, is_active) values('A voyage to the center of Burkina Fasso', 'Visit all the places 
+of this top country', 35000, 'Burkina Fasso', 1, true);
+
+insert into purchases(purchase_date, total_price, id_excursion, id_user) values(NOW(), 37000, 1, 1);
+insert into users_x_excursions(id_user, id_excursion, rating) values(1, 1, 5);
