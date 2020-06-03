c6d916d6f98466fb169bdaa4da246dd533d384ad
Matias Nicoletti <matiasnicolettmdp@gmail.com>
2020-06-03 00:03:08 -0300

    Nav and header

--- ExoTourierApp/src/app/app.module.ts
+++ ExoTourierApp/src/app/app.module.ts
@@ -9,13 +9,15 @@ import { RoutingModule } from './core/routing/routing.module';
 import { HttpService } from './core/http/http.service';
 import { UserService } from './core/services/user.service';
 import { HttpClientModule } from '@angular/common/http';
+import { HeaderComponent } from './layout/header/header.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     FooterComponent,
-    NavComponent
+    NavComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
---
+++ ExoTourierApp/src/app/layout/header/header.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['../../styles/layout/_header.scss']
+})
+export class HeaderComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
